09e668ed3ad713d00bac98b1f2709955bd7e0995
MohsinMalik2 <malikmuhsin70@gmail.com>
2022-03-17 18:46:23 +0500

    debug

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,21 @@
 </template>
 
 <script>
-import UserCard from './components/UserCard.vue'
+    import UserCard from './components/UserCard.vue'
 
     export default {
-  components: { UserCard },
-      name: 'App'
+        components: { UserCard },
+        name: 'App'
     }
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-    min-height:100vh;
-    background-color: #F3F5FA;
-  }
+    #app {
+      font-family: Avenir, Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      color: #2c3e50;
+      min-height:100vh;
+      background-color: #F3F5FA;
+    }
 </style>
